3b6f45034aba6f049b0fec6891f96e1409ae9aad
till <till@php.net>
2012-03-25 17:58:03 +0200

    * adding another test to see how well structures are passed

--- tests/Test/IniParserTest.php
+++ tests/Test/IniParserTest.php
@@ -42,4 +42,24 @@ EOF;
     {
         new IniParser('/this/should/never/exist.ini');
     }
+
+    public function testParser()
+    {
+        $file   = BASE_DIR . '/tests/fixtures/fixture01.ini';
+        $parser = new IniParser($file);
+
+        $config = $parser->parse();
+
+        $this->assertArrayHasKey('production', $config);
+
+        $productionConfig = $config['production'];
+
+        $this->assertArrayHasKey('hello', $productionConfig);
+        $this->assertArrayHasKey('super', $productionConfig);
+
+        $super = $productionConfig['super'];
+
+        $this->assertArrayHasKey('funny', $super);
+        $this->assertEquals('config', $super['funny']);
+    }
 }
