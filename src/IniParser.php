f43c24f3c9c1e7770ba728e796fa862e6132b443
till <till@php.net>
2012-03-25 17:41:54 +0200

    * cleanup CS  * update docblocks (they did not make sense at all)

--- src/IniParser.php
+++ src/IniParser.php
@@ -12,17 +12,21 @@
  * @author Austin Hyde
  */
 
-class IniParser {
-    public function __construct() {
+class IniParser
+{
+    public function __construct()
+    {
 
     }
+
     /**
      * Parses an INI file
      *
      * @param string $file
-     * @return void
+     * @return array
      */
-    public function parse($file) {
+    public function parse($file)
+    {
         return $this->process(file_get_contents($file));
     }
 
@@ -30,46 +34,67 @@ class IniParser {
      * Parses a string with INI contents
      *
      * @param string $src
-     * @return void
+     *
+     * @return array
      */
-    public function process($src) {
-        $simple_parsed = parse_ini_string($src, true);
+    public function process($src)
+    {
+        $simple_parsed      = parse_ini_string($src, true);
         $inheritance_parsed = array();
         foreach ($simple_parsed as $k=>$v) {
-            if (strpos($k,':')!==false) {
-                $sects = array_map('trim',array_reverse(explode(':',$k)));
-                $root = array_pop($sects);
-                $arr = $v;
-                foreach ($sects as $s) {
-                    $arr = array_merge($inheritance_parsed[$s],$arr);
-                }
-                $inheritance_parsed[$root] = $arr;
-            } else {
+            if (false === strpos($k,':')) {
                 $inheritance_parsed[$k] = $v;
+                continue;
             }
+            $sects = array_map('trim',array_reverse(explode(':',$k)));
+            $root = array_pop($sects);
+            $arr = $v;
+            foreach ($sects as $s) {
+                $arr = array_merge($inheritance_parsed[$s],$arr);
+            }
+            $inheritance_parsed[$root] = $arr;
         }
         return $this->parse_keys($inheritance_parsed);
     }
 
-    private function parse_keys($arr) {
+    /**
+     * @param array $arr
+     *
+     * @return array
+     */
+    private function parse_keys(array $arr)
+    {
         $output = array();
         foreach ($arr as $k=>$v) {
             if (is_array($v)) { // is a section
                 $output[$k] = $this->parse_keys($v);
-            } else { // not a section
-                $v = $this->parse_value($v);
-                if (strpos($k,'.')===false) $output[$k] = $v;
-                else $output = $this->rec_keys(explode('.',$k),$v,$output);
+                continue;
+            }
+
+            // not a section
+            $v = $this->parse_value($v);
+            if (strpos($k,'.')===false) {
+                $output[$k] = $v;
+            } else {
+                $output = $this->rec_keys(explode('.',$k),$v,$output);
             }
         }
 
         return $output;
     }
-    private function rec_keys($keys,$value,$parent) {
-        if (!$keys) return $value;
+
+    protected function rec_keys($keys,$value,$parent)
+    {
+        if (!$keys) {
+            return $value;
+        }
+
         $k = $this->parse_value(array_shift($keys));
-        if (!array_key_exists($k,$parent)) $parent[$k] = array();
-        $v = $this->rec_keys($keys,$value,$parent[$k]);
+        if (!array_key_exists($k,$parent)) {
+            $parent[$k] = array();
+        }
+
+        $v          = $this->rec_keys($keys,$value,$parent[$k]);
         $parent[$k] = $v;
         return $parent;
     }
@@ -78,9 +103,10 @@ class IniParser {
      * Parses and formats the key in a key-value pair
      *
      * @param string $key
-     * @return void
+     * @return string
      */
-    protected function parse_key($key) {
+    protected function parse_key($key)
+    {
         return $key;
     }
 
@@ -90,7 +116,8 @@ class IniParser {
      * @param string $value
      * @return void
      */
-    protected function parse_value($value) {
+    protected function parse_value($value)
+    {
         if (preg_match('/\[\s*.*?(?:\s*,\s*.*?)*\s*\]/',$v)) {
             return explode(',',trim(preg_replace('/\s+/','',$v),'[]'));
         }
