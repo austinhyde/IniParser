f3c371c77eb0d9c2a0d5e5e08a4d4c6960b61858
Javier Marín Ros <javier@marinros.com>
2013-02-18 16:12:21 +0100

    Support for PHP 5.2, optional use of ArrayObject

--- src/IniParser.php
+++ src/IniParser.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * [MIT Licensed](http://www.opensource.org/licenses/mit-license.php)
  * 
@@ -7,13 +8,13 @@
  * * Property nesting
  * * Simple arrays
  * 
- * Compatible with PHP 5.3.0+
+ * Compatible with PHP 5.2.0+
  * 
  * @author Austin Hyde
  * @author Till Klampaeckel <till@php.net>
  */
-class IniParser
-{
+class IniParser {
+
     /**
      * Filename of our .ini file.
      * @var string
@@ -21,12 +22,16 @@ class IniParser
     protected $file;
 
     /**
+     * @var boolean 
+     */
+    public $use_array_object = TRUE;
+
+    /**
      * @param string $file
      *
-     * @return \IniParser
+     * @return IniParser
      */
-    public function __construct($file = null)
-    {
+    public function __construct($file = null) {
         if ($file !== null) {
             $this->setFile($file);
         }
@@ -38,15 +43,17 @@ class IniParser
      * @param string $file
      * @return array
      */
-    public function parse($file = null)
-    {
+    public function parse($file = null) {
         if ($file !== null) {
             $this->setFile($file);
         }
         if (empty($this->file)) {
-            throw new \LogicException("Need a file to parse.");
+            throw new LogicException("Need a file to parse.");
         }
-        return $this->process(file_get_contents($this->file));
+
+        $simple_parsed = parse_ini_file($this->file, true);
+        $inheritance_parsed = $this->parseSections($simple_parsed);
+        return $this->parseKeys($inheritance_parsed);
     }
 
     /**
@@ -56,9 +63,8 @@ class IniParser
      *
      * @return array
      */
-    public function process($src)
-    {
-        $simple_parsed      = parse_ini_string($src, true);
+    public function process($src) {
+        $simple_parsed = parse_ini_string($src, true);
         $inheritance_parsed = $this->parseSections($simple_parsed);
         return $this->parseKeys($inheritance_parsed);
     }
@@ -66,13 +72,12 @@ class IniParser
     /**
      * @param string $file
      *
-     * @return \IniParser
-     * @throws \InvalidArgumentException
+     * @return IniParser
+     * @throws InvalidArgumentException
      */
-    public function setFile($file)
-    {
+    public function setFile($file) {
         if (!file_exists($file) || !is_readable($file)) {
-            throw new \InvalidArgumentException("The file '{$file}' cannot be opened.");
+            throw new InvalidArgumentException("The file '{$file}' cannot be opened.");
         }
         $this->file = $file;
         return $this;
@@ -83,12 +88,11 @@ class IniParser
      * @param  array  $simple_parsed
      * @return array  Parsed sections
      */
-    private function parseSections(array $simple_parsed)
-    {
+    private function parseSections(array $simple_parsed) {
         // do an initial pass to gather section names
         $sections = array();
         $globals = array();
-        foreach ($simple_parsed as $k=>$v) {
+        foreach ($simple_parsed as $k => $v) {
             if (is_array($v)) {
                 // $k is a section name
                 $sections[$k] = $v;
@@ -98,21 +102,21 @@ class IniParser
         }
 
         // now for each section, see if it uses inheritance
-        foreach ($sections as $k=>$v) {
-            if (false === strpos($k,':')) {
+        foreach ($sections as $k => $v) {
+            if (false === strpos($k, ':')) {
                 continue;
             }
 
-            $sects = array_map('trim',array_reverse(explode(':',$k)));
-            $root  = array_pop($sects);
-            $arr   = $v;
+            $sects = array_map('trim', array_reverse(explode(':', $k)));
+            $root = array_pop($sects);
+            $arr = $v;
             foreach ($sects as $s) {
                 if ($s === '^') {
                     $arr = array_merge($globals, $arr);
                 } elseif (array_key_exists($s, $sections)) {
-                    $arr = array_merge($sections[$s],$arr);
+                    $arr = array_merge($sections[$s], $arr);
                 } else {
-                    throw new \UnexpectedValueException("IniParser: In file '{$this->file}', section '{$root}': Cannot inherit from unknown section '{$s}'");
+                    throw new UnexpectedValueException("IniParser: In file '{$this->file}', section '{$root}': Cannot inherit from unknown section '{$s}'");
                 }
             }
             $sections[$root] = $arr;
@@ -127,10 +131,9 @@ class IniParser
      *
      * @return array
      */
-    private function parseKeys(array $arr)
-    {
-        $output = new \ArrayObject(array(), \ArrayObject::ARRAY_AS_PROPS);
-        foreach ($arr as $k=>$v) {
+    private function parseKeys(array $arr) {
+        $output = $this->use_array_object ? new ArrayObject(array(), ArrayObject::ARRAY_AS_PROPS) : array();
+        foreach ($arr as $k => $v) {
             if (is_array($v)) {
                 // this element represents a section; recursively parse the value
                 $output[$k] = $this->parseKeys($v);
@@ -138,7 +141,7 @@ class IniParser
                 // value is just a value
                 // if the key ends in a +, it means we should append to the previous value, if applicable
                 $append = false;
-                if (preg_match('/\s*\+\s*$/',$k) > 0) {
+                if (preg_match('/\s*\+\s*$/', $k) > 0) {
                     $k = preg_replace('/\s*\+\s*$/', '', $k);
                     $append = true;
                 }
@@ -146,16 +149,16 @@ class IniParser
                 // transform "a.b.c = x" into $output[a][b][c] = x
                 $path = explode('.', $k);
 
-                $current =& $output;
+                $current = & $output;
                 while (($current_key = array_shift($path))) {
                     if (!array_key_exists($current_key, $current)) {
                         if (!empty($path)) {
-                            $current[$current_key] = new \ArrayObject(array(), \ArrayObject::ARRAY_AS_PROPS);
+                            $current[$current_key] = $this->use_array_object ? new ArrayObject(array(), ArrayObject::ARRAY_AS_PROPS) : array();
                         } else {
                             $current[$current_key] = null;
                         }
                     }
-                    $current =& $current[$current_key];
+                    $current = & $current[$current_key];
                 }
 
                 $value = $this->parseValue($v);
@@ -163,7 +166,7 @@ class IniParser
                 if ($append && $current !== null) {
                     if (is_array($value)) {
                         if (!is_array($current)) {
-                            throw new \LogicException("Cannot append array to inherited value '{$k}'");
+                            throw new LogicException("Cannot append array to inherited value '{$k}'");
                         }
                         $value = array_merge($current, $value);
                     } else {
@@ -185,12 +188,12 @@ class IniParser
      *
      * @return mixed
      */
-    protected function parseValue($value)
-    {
+    protected function parseValue($value) {
         // if the value looks like [a,b,c,...], interpret as array
-        if (preg_match('/\[\s*.*?(?:\s*,\s*.*?)*\s*\]/',$value) > 0) {
-            return explode(',',trim(preg_replace('/\s+/','',$value),'[]'));
+        if (preg_match('/\[\s*.*?(?:\s*,\s*.*?)*\s*\]/', $value) > 0) {
+            return explode(',', trim(preg_replace('/\s+/', '', $value), '[]'));
         }
         return $value;
     }
+
 }
